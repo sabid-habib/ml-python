94925b5e26511fd3900ffefa7d30110e39e675be
sabid-habib <sabidbinhabib@gmail.com>
2018-08-25 23:44:41 +0600

    visualize melbourne data

--- kaggle-first/FirstModel.py
+++ kaggle-first/FirstModel.py
@@ -1,1 +1,10 @@
 import pandas as pd
+
+melbourne_file_path = 'melb_data.csv'
+melbourne_data = pd.read_csv(melbourne_file_path)
+# print(melbourne_data.columns)
+melbourne_data = melbourne_data.dropna(axis=0)
+y=melbourne_data.Price
+melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude']
+X = melbourne_data[melbourne_features]
+print(X.head())
