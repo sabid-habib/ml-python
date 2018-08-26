deaa472bcbf2bafb0e138061619d273f466f687f
sabid-habib <sabidbinhabib@gmail.com>
2018-08-26 23:52:24 +0600

    predictive model using sklearn

--- kaggle-first/FirstModel.py
+++ kaggle-first/FirstModel.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
 
 melbourne_file_path = 'melb_data.csv'
 melbourne_data = pd.read_csv(melbourne_file_path)
@@ -7,4 +8,11 @@ melbourne_data = melbourne_data.dropna(axis=0)
 y=melbourne_data.Price
 melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude']
 X = melbourne_data[melbourne_features]
+#print(X.head())
+melbourne_model = DecisionTreeRegressor(random_state=1)
+melbourne_model.fit(X, y)
+#print(melbourne_model)
+print("Making predictions for the following 5 houses:")
 print(X.head())
+print("The predictions are")
+print(melbourne_model.predict(X.head()))
